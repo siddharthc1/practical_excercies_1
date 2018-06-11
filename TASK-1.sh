a99873be5d0e44230070ced6b9341ec49a92abfa
siddharthc1 <39838707+siddharthc1@users.noreply.github.com>
2018-06-11 09:41:38 -0700

    Update TASK-1.sh

--- TASK-1.sh
+++ TASK-1.sh
@@ -5,3 +5,6 @@
 ​hive -e 'CREATE DATABASE practical_excercise_1;'
 sqoop import --connect jdbc:mysql://localhost/practical_exercise_1 --username root --password cloudera --table user -m 1 --hive-import --hive-database practical_excercise_1 --hive-table user 
 
+
+
+sqoop import --connect jdbc:mysql://localhost/practical_exercise_1 --username root --password cloudera --table activitylog -m 1 --hive-import --hive-database practical_excercise_1 --hive-table activitylog
