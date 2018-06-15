a671762725753c222296284dc372198bec3670d2
siddharthc1 <39838707+siddharthc1@users.noreply.github.com>
2018-06-15 13:34:43 -0700

    Update TASK-1.sh

--- TASK-1.sh
+++ TASK-1.sh
@@ -1,5 +1,3 @@
-
-
 #!/bin/sh
 
 
@@ -7,33 +5,48 @@ n=1
 #loop running for 3 times
 
 
-#printf "cloudera" > root_pwd.txt
-#hadoop fs -put root_pwd.txt /user/cloudera/
-#hadoop fs -chmod 400 /user/cloudera/root_pwd.txt
-#nohup sqoop metastore &
+nohup sqoop metastore &
 
 
 while  [ $n -le 3 ]
 
 do
-	hive -e "create database if not exists practical_excercise_1;"
-	sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop --create practical_excercise_1.user -- import --connect jdbc:mysql://localhost/practical_exercise_1 --username root --password-file /user/cloudera/root_pwd.txt --table user -m 1 --hive-import --hive-database practical_excercise_1 --hive-table user --incremental append --check-column id --last-value 0
+	hive -e "create database if not exists practical_exercise_1;"
+
+	sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop --create practical_exercise_1.user -- import --connect jdbc:mysql://localhost/practical_exercise_1 --username root --password-file /user/cloudera/root_pwd.txt --table user -m 4 --hive-import --hive-database practical_exercise_1 --hive-table user --incremental append --check-column id --last-value 0
+	
+	if [ $? -ne 0 ];then
+		echo Failed at creating user table at hive 
+		exit 1
+		continue
+	fi
 
 
-	sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop --exec practical_excercise_1.user
+	sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop --exec practical_exercise_1.user
+
+	if [ $? -ne 0 ];then
+		echo Failed at creating user table at hive 
+		exit 1
+		continue
+	fi
 
 
-	sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop --create practical_excercise_1.activitylog -- import --connect jdbc:mysql://localhost/practical_exercise_1 --username root --password-file /user/cloudera/root_pwd.txt --table activitylog -m 1 --hive-import --hive-database practical_excercise_1 --hive-table activitylog --incremental append --check-column id --last-value 0
+	sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop --create practical_exercise_1.activitylog -- import --connect jdbc:mysql://localhost/practical_exercise_1 --username root --password-file /user/cloudera/root_pwd.txt --table activitylog -m 4 --hive-import --hive-database practical_exercise_1 --hive-table activitylog --incremental append --check-column id --last-value 0
+	
+	if [ $? -ne 0 ];then
+		echo Failed at creating activitylog table at hive
+		exit 1
+		continue
+	fi
 
-	sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop --exec practical_excercise_1.activitylog
+	sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop --exec practical_exercise_1.activitylog
 
-	if [ $? -eq 0 ]
-	then
-		echo Successful
-		exit 0
-		break
+	if [ $? -ne 0 ];then
+		echo Failed at creating activitylog table at hive 
+		exit 1
+		continue
 	else
-		echo Process Failed
+		break
 	fi
 
 	n=$(( n+1 ))
