328c46d3d4e6ab5170f7effa01a9e0892b03c10b
siddharthc1 <39838707+siddharthc1@users.noreply.github.com>
2018-06-18 10:01:22 -0700

    Update TASK-2.sh

--- TASK-2.sh
+++ TASK-2.sh
@@ -1,13 +1,60 @@
 #!/bin/sh
-echo 'TASK-2'
 
-hadoop fs -mkdir /user/cloudera/workshop/hive1/ 
-hadoop fs -mkdir /user/cloudera/workshop/hive1/logs/
-hadoop fs -mkdir /user/cloudera/workshop/hive1/logs/input/
 
-hadoop fs -put user_upload_dump.csv /user/cloudera/workshop/hive1/logs/input/
+n=1
+#loop running for 3 times
 
-hive -e "CREATE EXTERNAL TABLE user_upload_dump1 ( user_id int, file_name STRING, timestamp int) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' STORED AS TEXTFILE LOCATION '/user/cloudera/workshop/hive1/logs/input' tblproperties ('skip.header.line.count'='1');"
+while  [ $n -le 2 ]
+
+do
+	echo 'TASK-2'
+
+	hadoop fs -mkdir /user/cloudera/workshop/process/ 						 #initialisation script
+	hadoop fs -mkdir /user/cloudera/workshop/archieve/                                              #initialisation script
+ 
+	name=$(basename user_upload_dum*.csv)
+	echo $name
+	#mv $name user_upload_dump.csv
+	hadoop fs -put $name /user/cloudera/workshop/process/
+	
+	if [ $? -ne 0 ];then
+		echo Failed at uploading files
+		exit 1
+		continue
+	fi	
+
+	hive -e "CREATE EXTERNAL TABLE practical_exercise_1.user_upload_dump ( user_id int, file_name STRING, timestamp int) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' STORED AS TEXTFILE LOCATION '/user/cloudera/workshop/process/' tblproperties ('skip.header.line.count'='1');"
+        
+	if [ $? -ne 0 ];then
+		echo Failed at external table
+		exit 1
+		continue
+	fi	
+	
+	hadoop fs -mv  /user/cloudera/workshop/process/* /user/cloudera/workshop/archieve/
+	
+	if [ $? -ne 0 ];then
+		echo Failed  
+		exit 1
+		continue
+	fi
+
+	hadoop fs -rm -r  /user/cloudera/workshop/process/
+
+	if [ $? -ne 0 ];then
+		echo Failed at creating user table at hive 
+		exit 1
+		continue
+	else
+		break
+	
+	fi
+
+	n=$(( n+1 ))
+
+
+done
+
 
 
 
