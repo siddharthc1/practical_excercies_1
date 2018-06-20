b351ff924d5874fd5f7900d03ef114f5379dbaa6
siddharthc1 <39838707+siddharthc1@users.noreply.github.com>
2018-06-20 11:17:50 -0700

    Update MySQL_to_HIVE.sh

--- MySQL_to_HIVE.sh
+++ MySQL_to_HIVE.sh
@@ -1,37 +1,19 @@
 #!/bin/sh
+#TASK-1: Load the files from the mysql database to hive using sqoop 
 
-
-n=1
-#loop running for 3 times
-
-
-nohup sqoop metastore &
-
-
-while  [ $n -le 3 ]
-
-do
+#sqoop import table user from mysql to hive database
+sqoop import --connect jdbc:mysql://localhost/practical_exercise_1 --username root --password-file /user/cloudera/root_pwd.txt --table user -m 4 --hive-import --hive-database practical_exercise_1 --hive-table user 
 	
-	sqoop import --connect jdbc:mysql://localhost/practical_exercise_1 --username root --password-file /user/cloudera/root_pwd.txt --table user -m 4 --hive-import --hive-database practical_exercise_1 --hive-table user 
+if [ $? -ne 0 ];then
+	echo Failed at creating user table at hive 
+	exit 1
+fi
+
+#sqoop import table activitylog from mysql to hive database 
+sqoop import --connect jdbc:mysql://localhost/practical_exercise_1 --username root --password-file /user/cloudera/root_pwd.txt --table activitylog -m 4 --hive-import --hive-database practical_exercise_1 --hive-table activitylog 
 	
-	if [ $? -ne 0 ];then
-		echo Failed at creating user table at hive 
-		exit 1
-		continue
-	fi
-
-	sqoop import --connect jdbc:mysql://localhost/practical_exercise_1 --username root --password-file /user/cloudera/root_pwd.txt --table activitylog -m 4 --hive-import --hive-database practical_exercise_1 --hive-table activitylog 
-	
-	if [ $? -ne 0 ];then
-		echo Failed at creating activitylog table at hive
-		exit 1
-		continue
-	else
-		break
-	fi
-
-	n=$(( n+1 ))
-
-
-done
+if [ $? -ne 0 ];then
+	echo Failed at creating activitylog table at hive
+	exit 1
+fi
 
