0dfd1ee383fc30357be6af5ed9b3dba638278c28
siddharthc1 <39838707+siddharthc1@users.noreply.github.com>
2018-06-20 15:37:30 -0700

    Update MySQL_to_HIVE.sh

--- MySQL_to_HIVE.sh
+++ MySQL_to_HIVE.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 #TASK-1: Load the files from the mysql database to hive using sqoop 
 
+#generating the data from generator file 
+python practical_exercise_data_generator.py --load_data
+python practical_exercise_data_generator.py --create_csv
+
 #sqoop import table user from mysql to hive 
-sqoop import --connect jdbc:mysql://localhost/practical_exercise_1 --username root --password-file /user/cloudera/root_pwd.txt --table user -m 4 --hive-import --hive-database practical_exercise_1 --hive-table user 
+sqoop import --connect jdbc:mysql://localhost/practical_exercise_1 --username root --password-file /user/cloudera/root_pwd.txt --table user -m 4 --hive-import --hive-overwrite --hive-database practical_exercise_1 --hive-table user
 	
 if [ $? -ne 0 ];then
 	echo Failed at creating user table at hive 
@@ -10,7 +14,7 @@ if [ $? -ne 0 ];then
 fi
 
 #sqoop import table activitylog from mysql to hive  
-sqoop import --connect jdbc:mysql://localhost/practical_exercise_1 --username root --password-file /user/cloudera/root_pwd.txt --table activitylog -m 4 --hive-import --hive-database practical_exercise_1 --hive-table activitylog 
+sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop --exec practical_exercise_1.activitylog
 	
 if [ $? -ne 0 ];then
 	echo Failed at creating activitylog table at hive
