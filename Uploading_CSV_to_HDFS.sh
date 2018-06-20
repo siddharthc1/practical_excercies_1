eb19848f1955912ddff01f14470800d8c3baaa57
siddharthc1 <39838707+siddharthc1@users.noreply.github.com>
2018-06-20 15:38:26 -0700

    Update Uploading_CSV_to_HDFS.sh

--- Uploading_CSV_to_HDFS.sh
+++ Uploading_CSV_to_HDFS.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 #TASK-2: Upload the CSV file on HDFS to a directory which is pointed by the external table and the move the files to archieve folder to make it a backup
 
-#get the name of the file created by the practical_exercise_data_generator.py	
-name=$(basename user_upload_dum*.csv)
 
 #upload the files to HDFS	
-hadoop fs -put $name /user/cloudera/workshop/process/
+hadoop fs -put *.csv /user/cloudera/workshop/process/
 	
 if [ $? -ne 0 ];then
 	echo Failed at uploading files
