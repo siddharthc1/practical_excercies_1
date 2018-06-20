b77c0e2e68de7d3e4c00f43833c8a90548225bf0
siddharthc1 <39838707+siddharthc1@users.noreply.github.com>
2018-06-20 11:15:23 -0700

    Update initialise.sh

--- initialise.sh
+++ initialise.sh
@@ -4,6 +4,9 @@
 hadoop fs -rm -r /user/cloudera/user
 hadoop fs -rm -r /user/cloudera/activitylog
 
+#starting sqoop metajob
+nohup sqoop metastore &
+
 #deleting the sqoop job 
 sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop --delete practical_exercise_1.activitylog
 
@@ -16,7 +19,7 @@ hive -e "drop database practical_exercise_1 cascade;"
 #creating database with name practical_exercise_1
 hive -e "create database practical_exercise_1;"
 
-#creating sqoop meta job for deltas for task_1
+#creating sqoop meta job for deltas for task_1 for activitylog table
 sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop --create practical_exercise_1.activitylog -- import --connect jdbc:mysql://localhost/practical_exercise_1 --username root --password-file /user/cloudera/root_pwd.txt --table activitylog -m 4 --hive-import --hive-database practical_exercise_1 --hive-table activitylog --incremental append --check-column id --last-value 0
 
 sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop --exec practical_exercise_1.activitylog
