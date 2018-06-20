6718402da051ba1b60daa28999734aea18b5f43b
siddharthc1 <39838707+siddharthc1@users.noreply.github.com>
2018-06-20 15:39:05 -0700

    Update Generating_UserReport_UserTotal.sh

--- Generating_UserReport_UserTotal.sh
+++ Generating_UserReport_UserTotal.sh
@@ -1,45 +1,30 @@
 #!/bin/sh
 
-n=1
-#loop running for 3 times
+NOW=$(date +%s)
 
-while  [ $n -le 3 ]
+#drop table already existing user report table
+hive -e "drop table if exists practical_exercise_1.user_report;"
 
-do
+#hive -e "set hive.cli.print.header=true"
 
-	NOW=$(date +%s)
- 
-	echo 'TASK_3' 
+hive -e "create table practical_exercise_1.user_report(user_id int, total_updates int, total_inserts int, total_deletes int, last_activity_type string, is_active boolean, upload_count int);"
 
-	hive -e "show databases;"
-	
-	hive -e "show tables in practical_exercise_1 ;"
+hive -e "insert into practical_exercise_1.user_report select a.user_id, COALESCE(b.co,0) as update, COALESCE(c.co,0) as insert, COALESCE(d.co,0) as delete, COALESCE(e.co,NULL) as type, COALESCE(f.co,FALSE) as bool, COALESCE(g.co,0) as upload from (select user_id from practical_exercise_1.activitylog group by user_id) as a left join (select user_id, count(user_id) as co from practical_exercise_1.activitylog where type='UPDATE' group by user_id) as b on a.user_id=b.user_id  left join (select user_id, count(user_id) as co from practical_exercise_1.activitylog where type='INSERT' group by user_id) as c on a.user_id=c.user_id left join(select user_id, count(user_id) as co from practical_exercise_1.activitylog where type='DELETE' group by user_id) as d on a.user_id=d.user_id left join (SELECT a.user_id, a.type as co FROM practical_exercise_1.activitylog a INNER JOIN (SELECT user_id, MIN(ti) as ti FROM practical_exercise_1.activitylog GROUP BY user_id ) AS b ON a.user_id = b.user_id AND a.ti = b.ti) as e on a.user_id=e.user_id left join (select user_id, if(count(*) = 0, FALSE, TRUE) as co from practical_exercise_1.activitylog where ti between $NOW AND $NOW-172800 group by user_id) as f on a.user_id=f.user_id left join (select user_id, count(user_id) as co from practical_exercise_1.user_upload_dump group by user_id) as g on a.user_id=g.user_id;"
 
-	hive -e "drop table practical_exercise_1.user_report;"
+if [ $? -ne 0 ];then
+	echo Failed at creating user_report
+	exit 1
+fi
 
-	hive -e "create table practical_exercise_1.user_report as select a.user_id, COALESCE(b.co,0) as update, COALESCE(c.co,0) as insert, COALESCE(d.co,0) as delete, COALESCE(e.co,NULL) as type, COALESCE(f.co,FALSE) as bool, COALESCE(g.co,0) as upload from (select user_id from practical_exercise_1.activitylog group by user_id) as a left join (select user_id, count(user_id) as co from practical_exercise_1.activitylog where type='UPDATE' group by user_id) as b on a.user_id=b.user_id  left join (select user_id, count(user_id) as co from practical_exercise_1.activitylog where type='INSERT' group by user_id) as c on a.user_id=c.user_id left join(select user_id, count(user_id) as co from practical_exercise_1.activitylog where type='DELETE' group by user_id) as d on a.user_id=d.user_id left join (SELECT a.user_id, a.type as co FROM practical_exercise_1.activitylog a INNER JOIN (SELECT user_id, MIN(timestamp) as timestamp FROM practical_exercise_1.activitylog GROUP BY user_id ) AS b ON a.user_id = b.user_id AND a.timestamp = b.timestamp) as e on a.user_id=e.user_id left join (select user_id, if(count(*) = 0, FALSE, TRUE) as co from practical_exercise_1.activitylog where timestamp between $NOW AND $NOW-172800 group by user_id) as f on a.user_id=f.user_id left join (select user_id, count(user_id) as co from practical_exercise_1.user_upload_dump group by user_id) as g on a.user_id=g.user_id;"
+hive -e "insert into practical_exercise_1.user_total select $NOW, sub1.t , case when sub2.t1 is NULL then sub1.t when sub2.t1 is not NULL then sub1.t-sub2.t1 end from (select count(distinct id) as t from practical_exercise_1.user)sub1, (select max(total_users) t1 from practical_exercise_1.user_total) sub2;"
 
-	if [ $? -ne 0 ];then
-		echo Failed at creating user_report
-		exit 1
-		continue
-	fi
-
-	hive -e "insert into practical_exercise_1.user_total select $NOW, sub1.t , case when sub2.t1 is NULL then sub1.t when sub2.t1 is not NULL then sub1.t-sub2.t1 end from (select count(distinct id) as t from practical_exercise_1.user)sub1, (select max(total_users) t1 from practical_exercise_1.user_total) sub2;"
-
-	if [ $? -ne 0 ];then
-		echo Failed at inserting into user_total
-		exit 1
-		continue
-	else
-		break
-	fi
-
-	n=$((n+1))
+if [ $? -ne 0 ];then
+	echo Failed at inserting into user_total
+	exit 1
+fi
 
 
-done
-
-
-
+hive -e "set hive.cli.print.header=true;"
+hive -e "select * from user_report;"
+hive -e "select * from user_total;"
 
