580d17a4884ae6a5b21971492c38a3f9cf0c2122
siddharthc1 <39838707+siddharthc1@users.noreply.github.com>
2018-06-21 11:50:47 -0700

    Update MySQL_to_HIVE.sh

--- MySQL_to_HIVE.sh
+++ MySQL_to_HIVE.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-#TASK-1: Load the files from the mysql database to hive using sqoop 
+#TASK-1: Load the files from the MySQL database to Hive using Sqoop 
 
-#generating the data from generator file 
+#Generating the data from generator file 
 python practical_exercise_data_generator.py --load_data
 python practical_exercise_data_generator.py --create_csv
 
-#sqoop import table user from mysql to hive 
+#Sqoop import table user from MySQL to Hive 
 sqoop import --connect jdbc:mysql://localhost/practical_exercise_1 --username root --password-file /user/cloudera/root_pwd.txt --table user -m 4 --hive-import --hive-overwrite --hive-database practical_exercise_1 --hive-table user
 	
 if [ $? -ne 0 ];then
@@ -13,7 +13,7 @@ if [ $? -ne 0 ];then
 	exit 1
 fi
 
-#sqoop import table activitylog from mysql to hive  
+#Sqoop import table activitylog from MySQL to Hive  
 sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop --exec practical_exercise_1.activitylog
 	
 if [ $? -ne 0 ];then
