a76d1e58324dff18e6353880b9a24ee22aed73bb
siddharthc1 <39838707+siddharthc1@users.noreply.github.com>
2018-06-21 09:35:17 -0700

    Update Generating_UserReport_UserTotal.sh

--- Generating_UserReport_UserTotal.sh
+++ Generating_UserReport_UserTotal.sh
@@ -5,8 +5,6 @@ NOW=$(date +%s)
 #drop table already existing user report table
 hive -e "drop table if exists practical_exercise_1.user_report;"
 
-#hive -e "set hive.cli.print.header=true"
-
 hive -e "create table practical_exercise_1.user_report(user_id int, total_updates int, total_inserts int, total_deletes int, last_activity_type string, is_active boolean, upload_count int);"
 
 hive -e "insert into practical_exercise_1.user_report select a.user_id, COALESCE(b.co,0) as update, COALESCE(c.co,0) as insert, COALESCE(d.co,0) as delete, COALESCE(e.co,NULL) as type, COALESCE(f.co,FALSE) as bool, COALESCE(g.co,0) as upload from (select user_id from practical_exercise_1.activitylog group by user_id) as a left join (select user_id, count(user_id) as co from practical_exercise_1.activitylog where type='UPDATE' group by user_id) as b on a.user_id=b.user_id  left join (select user_id, count(user_id) as co from practical_exercise_1.activitylog where type='INSERT' group by user_id) as c on a.user_id=c.user_id left join(select user_id, count(user_id) as co from practical_exercise_1.activitylog where type='DELETE' group by user_id) as d on a.user_id=d.user_id left join (SELECT a.user_id, a.type as co FROM practical_exercise_1.activitylog a INNER JOIN (SELECT user_id, MIN(ti) as ti FROM practical_exercise_1.activitylog GROUP BY user_id ) AS b ON a.user_id = b.user_id AND a.ti = b.ti) as e on a.user_id=e.user_id left join (select user_id, if(count(*) = 0, FALSE, TRUE) as co from practical_exercise_1.activitylog where ti between $NOW AND $NOW-172800 group by user_id) as f on a.user_id=f.user_id left join (select user_id, count(user_id) as co from practical_exercise_1.user_upload_dump group by user_id) as g on a.user_id=g.user_id;"
@@ -22,9 +20,4 @@ if [ $? -ne 0 ];then
 	echo Failed at inserting into user_total
 	exit 1
 fi
-
-
-hive -e "set hive.cli.print.header=true;"
-hive -e "select * from user_report;"
-hive -e "select * from user_total;"
 
