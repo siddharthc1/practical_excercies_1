8ceb3a8a90ef846e3315cb2b5381b14739846d0f
siddharthc1 <39838707+siddharthc1@users.noreply.github.com>
2018-06-21 11:40:51 -0700

    Update Generating_UserReport_UserTotal.sh

--- Generating_UserReport_UserTotal.sh
+++ Generating_UserReport_UserTotal.sh
@@ -7,7 +7,21 @@ hive -e "drop table if exists practical_exercise_1.user_report;"
 
 hive -e "create table practical_exercise_1.user_report(user_id int, total_updates int, total_inserts int, total_deletes int, last_activity_type string, is_active boolean, upload_count int);"
 
-hive -e "insert into practical_exercise_1.user_report select a.user_id, COALESCE(b.co,0) as update, COALESCE(c.co,0) as insert, COALESCE(d.co,0) as delete, COALESCE(e.co,NULL) as type, COALESCE(f.co,FALSE) as bool, COALESCE(g.co,0) as upload from (select user_id from practical_exercise_1.activitylog group by user_id) as a left join (select user_id, count(user_id) as co from practical_exercise_1.activitylog where type='UPDATE' group by user_id) as b on a.user_id=b.user_id  left join (select user_id, count(user_id) as co from practical_exercise_1.activitylog where type='INSERT' group by user_id) as c on a.user_id=c.user_id left join(select user_id, count(user_id) as co from practical_exercise_1.activitylog where type='DELETE' group by user_id) as d on a.user_id=d.user_id left join (SELECT a.user_id, a.type as co FROM practical_exercise_1.activitylog a INNER JOIN (SELECT user_id, MIN(ti) as ti FROM practical_exercise_1.activitylog GROUP BY user_id ) AS b ON a.user_id = b.user_id AND a.ti = b.ti) as e on a.user_id=e.user_id left join (select user_id, if(count(*) = 0, FALSE, TRUE) as co from practical_exercise_1.activitylog where ti between $NOW AND $NOW-172800 group by user_id) as f on a.user_id=f.user_id left join (select user_id, count(user_id) as co from practical_exercise_1.user_upload_dump group by user_id) as g on a.user_id=g.user_id;"
+hive -e "insert into practical_exercise_1.user_report 
+select a.user_id, 
+COALESCE(b.co,0) as U, 
+COALESCE(c.co,0) as I, 
+COALESCE(d.co,0) as D, 
+COALESCE(e.co,NULL) as type, 
+COALESCE(f.co,FALSE) as bool, 
+COALESCE(g.co,0) as upload 
+from (select id as user_id from practical_exercise_1.user group by id) as a 
+left join (select user_id, count(user_id) as co from practical_exercise_1.activitylog where type='UPDATE' group by user_id) as b on a.user_id=b.user_id  
+left join (select user_id, count(user_id) as co from practical_exercise_1.activitylog where type='INSERT' group by user_id) as c on a.user_id=c.user_id 
+left join(select user_id, count(user_id) as co from practical_exercise_1.activitylog where type='DELETE' group by user_id) as d on a.user_id=d.user_id 
+left join (SELECT a.user_id, a.type as co FROM practical_exercise_1.activitylog a INNER JOIN (SELECT user_id, MAX(ti) as ti FROM practical_exercise_1.activitylog GROUP BY user_id ) AS b ON a.user_id = b.user_id AND a.ti = b.ti) as e on a.user_id=e.user_id 
+left join (select user_id, if(count(*) = 0, FALSE, TRUE) as co from practical_exercise_1.activitylog where ti between 1529351994 AND 1529351994-172800 group by user_id) as f on a.user_id=f.user_id 
+left join (select user_id, count(user_id) as co from practical_exercise_1.user_upload_dump group by user_id) as g on a.user_id=g.user_id;"
 
 if [ $? -ne 0 ];then
 	echo Failed at creating user_report
@@ -20,4 +34,5 @@ if [ $? -ne 0 ];then
 	echo Failed at inserting into user_total
 	exit 1
 fi
+
 
