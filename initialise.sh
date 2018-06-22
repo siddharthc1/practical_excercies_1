16b773b59942555c91fb909f6120a54cd7c48cdc
siddharthc1 <39838707+siddharthc1@users.noreply.github.com>
2018-06-22 09:24:22 -0700

    Update initialise.sh

--- initialise.sh
+++ initialise.sh
@@ -22,8 +22,6 @@ hive -e "create database practical_exercise_1;"
 #Creating sqoop meta job for deltas for task_1 for activitylog table
 sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop --create practical_exercise_1.activitylog -- import --connect jdbc:mysql://localhost/practical_exercise_1 --username root --password-file /user/cloudera/root_pwd.txt --table activitylog -m 4 --hive-import --hive-database practical_exercise_1 --hive-table activitylog --incremental append --check-column id --last-value 0
 
-sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop --exec practical_exercise_1.activitylog
-
 #Creating folders process and archieve inside workshop directory to store the csv file
 #External table points to process folder
 #Archieve folder will store the backup for process fail
